41c71a5c42fd5e137804564d9b0377636a4b58fa
Qafas <rustam_ussr@mail.ru>
2023-03-15 22:04:27 +0400

    4. Категории в TODO List [#331991 #401248]

---
+++ db/scripts/010_dml_insert_categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories (name) VALUES ('Разработка');
+INSERT INTO categories (name) VALUES ('Тестирование');
+INSERT INTO categories (name) VALUES ('Дизайн');
+INSERT INTO categories (name) VALUES ('Маркетинг');
+INSERT INTO categories (name) VALUES ('Эскизное проектирование');
+
+
+
+
--- src/main/java/ru/job4j/todo/model/Task.java
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tasks")
@@ -29,4 +31,12 @@ public class Task {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "priority_id")
     private Priority priority;
+
+    @ManyToMany
+    @JoinTable(
+            name = "task_categories",
+            joinColumns = { @JoinColumn(name = "task_id") },
+            inverseJoinColumns = { @JoinColumn(name = "category_id") }
+    )
+    private List<Category> taskCategories = new ArrayList<>();
 }
